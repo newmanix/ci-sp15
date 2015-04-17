5e3581565bae29accb0bc3816f11b3cedc2b02de
Bill Newman <william.newman@seattlecolleges.edu>
2015-04-16 17:53:16 -0700

    Adds News model and database

--- application/config/routes.php
+++ application/config/routes.php
@@ -53,9 +53,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-*/
+
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
+*/
+
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
+$route['(:any)'] = 'pages/view/$1';
+$route['default_controller'] = 'pages/view';
 
 
 
