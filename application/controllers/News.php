6ff6abcf564be0579b50c98599402cfe0b933395
Bill Newman <william.newman@seattlecolleges.edu>
2015-04-21 16:52:26 -0700

    Adds Add capability to News

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -32,6 +32,32 @@ class News extends CI_Controller {
 			$this->load->view('news/view', $data);
 			$this->load->view('templates/footer');
 	}
+    
+    
+    public function create()
+    {
+        $this->load->helper('form');
+        $this->load->library('form_validation');
+
+        $data['title'] = 'Create a news item';
+
+        $this->form_validation->set_rules('title', 'Title', 'required');
+        $this->form_validation->set_rules('text', 'text', 'required');
+
+        if ($this->form_validation->run() === FALSE)
+        {
+            $this->load->view('templates/header', $data);
+            $this->load->view('news/create', $data);
+            $this->load->view('templates/footer', $data);
+
+        }
+        else
+        {
+            $this->news_model->set_news();
+            $this->load->view('news/success');
+        }
+    }
+    
 }
 
 
