3c7e37c17df380f3d2b9b6d1de73179e00967061
newmanix <william.newman@seattlecolleges.edu>
2015-04-28 17:06:45 -0700

    Cleans up view.php in news

--- application/controllers/News.php
+++ application/controllers/News.php
@@ -1,24 +1,20 @@
 <?php
+//News.php controller
 class News extends CI_Controller {
 
-        public function __construct()
-        {
-                parent::__construct();
-             $this->load->model('news_model');
-             $this->config->set_item('banner','Global News Banner');
-        }
+    public function __construct()
+    {//everything here is global to all methods in the controller
+         parent::__construct();
+         $this->load->model('news_model');
+         $this->config->set_item('banner','Global News Banner');
+    }#end constructor()
 
 	public function index()
 	{
 			$data['news'] = $this->news_model->get_news();
 			$data['title'] = 'News archive';
-        
-            //$this->config->set_item('banner','News Banner');
-
-			//$this->load->view('templates/header', $data);
 			$this->load->view('news/index', $data);
-			//$this->load->view('templates/footer');
-	}
+	}#end index()
 
 	public function view($slug = NULL)
 	{
@@ -30,11 +26,8 @@ class News extends CI_Controller {
 			}
 
 			$data['title'] = $data['news_item']['title'];
-
-			$this->load->view('templates/header', $data);
 			$this->load->view('news/view', $data);
-			$this->load->view('templates/footer');
-	}
+	}#end view()
     
     
     public function create()
--- application/views/news/index.php
+++ application/views/news/index.php
@@ -1,5 +1,5 @@
 <?php
-//$this->load->view('themes/bootswatch/header');
+//views/news/index.php
 $this->load->view($this->config->item('theme') . 'header');
 ?>
 <h2><?php echo $title ?></h2>
@@ -10,10 +10,16 @@ $this->load->view($this->config->item('theme') . 'header');
         <div class="main">
                 <?php echo $news_item['text'] ?>
         </div>
-        <p><a href="<?php echo $news_item['slug'] ?>">View article</a></p>
+<p>
+<?php
+    echo anchor('news/' . $news_item['slug'],'View Article');
+?>
+</p>
+
+
+
 
 <?php endforeach ?>
 <?php
-//$this->load->view('themes/bootswatch/footer');
 $this->load->view($this->config->item('theme') . 'footer');
 ?>
